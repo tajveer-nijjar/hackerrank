eb0bf8acf18a6c06ce5611a0c95d15fa3144ff99
Taj <taj.nijjar2002@gmail.com>
2020-02-23 06:37:17 +1300

    Added sample file for first solution 1SimpleSumArray.

---
+++ C#/Algorithms/1SimpleSumArray.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace C_
+{
+    public class SimpleSumArray
+    {
+        public static void MainEntry()
+        {
+            Console.WriteLine("abc");
+        }
+    }
+}
--- C#/Program.cs
+++ C#/Program.cs
@@ -6,7 +6,8 @@ namespace C_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
+            SimpleSumArray.MainEntry();
         }
     }
 }
