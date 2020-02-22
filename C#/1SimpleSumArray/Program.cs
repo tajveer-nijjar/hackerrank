f86bd9a3917537f9a7302c5980966cf95b51516e
Taj <taj.nijjar2002@gmail.com>
2020-02-23 06:30:55 +1300

    Added first C# project.

---
+++ C#/1SimpleSumArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1SimpleSumArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
